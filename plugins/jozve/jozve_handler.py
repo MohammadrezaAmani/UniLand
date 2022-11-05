50ec1bbda5b1f8fa6882ce69b0bfde568c0f7722
Mohammadreza <more.amani@yahoo.com>
2022-11-05 21:51:56 +0330

    updating jozve_handler

--- plugins/jozve/jozve_handler.py
+++ plugins/jozve/jozve_handler.py
@@ -7,3 +7,23 @@ async def jozve_handler(client, message):
             text=messages.JOZVE_DESCRIPTION,
             reply_markup=pages.JOZVE
         )
+@Client.on_message(filters.regex(messages.JOZVE_SEARCH))
+async def jozve_search_handler(client, message):
+    await message.reply(
+            text=messages.JOZVE_SEARCH_DESCRIPTION,
+            reply_markup=pages.BACK
+        )
+
+@Client.on_message(filters.regex(messages.JOZVE_SABT))
+async def jozve_sabt_handler(client, message):
+    await message.reply(
+            text=messages.JOZVE_SABT_DESCRIPTION,
+            reply_markup=pages.JOZVE_SABT
+        )
+
+@Client.on_message(filters.regex(messages.JOZVE_DARKHASTI))
+async def jozve_handler(client, message):
+    await message.reply(
+            text=messages.JOZVE_DARKHASTI_DESCRIPTION,
+            reply_markup=pages.BACK
+        )
