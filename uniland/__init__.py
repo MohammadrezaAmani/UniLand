6f56e56bdc761c6ebe63c0d1b37dd4a16a8bc2ea
Mohammadreza Amani <More.amani@yahoo.com>
2022-12-23 00:15:46 +0330

    FORMATING __init__.py

--- uniland/__init__.py
+++ uniland/__init__.py
@@ -8,34 +8,38 @@ from .config import DB_URI
 
 
 def start() -> scoped_session:
-  # Creating db engine and session
-  engine = create_engine(DB_URI)
-  create_tables(engine)
-  session = scoped_session(
-    sessionmaker(bind=engine, autoflush=False, expire_on_commit=False))
-
-  # adding users
-  usercache = UserCache()
-  for user in session.query(User).all():
-    usercache.add_user(
-      user_id=user.user_id,
-      permission=user.access_level.value,
-      last_step=user.last_step,
+    # Creating db engine and session
+    engine = create_engine(DB_URI)
+    create_tables(engine)
+    session = scoped_session(
+        sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
     )
 
-  # Indexing all submissions
-  search_engine = SearchEngine()
-  for submission in session.query(Submission).all():
-    usercache.increase_achieved_likes(submission.owner_id,
-                                      len(submission.liked_users))
-    if submission.is_confirmed:
-      search_engine.index_record(id=submission.id,
-                                 search_text=submission.search_text,
-                                 sub_type=submission.submission_type,
-                                 likes=len(submission.liked_users),
-                                 search_times=submission.search_times)
+    # adding users
+    usercache = UserCache()
+    for user in session.query(User).all():
+        usercache.add_user(
+            user_id=user.user_id,
+            permission=user.access_level.value,
+            last_step=user.last_step,
+        )
 
-  return (session, search_engine, usercache)
+    # Indexing all submissions
+    search_engine = SearchEngine()
+    for submission in session.query(Submission).all():
+        usercache.increase_achieved_likes(
+            submission.owner_id, len(submission.liked_users)
+        )
+        if submission.is_confirmed:
+            search_engine.index_record(
+                id=submission.id,
+                search_text=submission.search_text,
+                sub_type=submission.submission_type,
+                likes=len(submission.liked_users),
+                search_times=submission.search_times,
+            )
+
+    return (session, search_engine, usercache)
 
 
 SESSION, search_engine, usercache = start()
