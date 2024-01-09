0469a6cc2fc1c0f099f8069457dabd3e8ff5d8ef
Mohammadreza Amani <More.Amani@yahoo.com>
2024-01-09 15:40:00 +0330

    minor changes

--- uniland/plugins/navigation/back_handler.py
+++ uniland/plugins/navigation/back_handler.py
@@ -10,6 +10,17 @@ from uniland.utils.steps import UserSteps
 
 @Client.on_message(filters.regex(triggers.Triggers.BACK.value) & filters.private)
 async def back_nav(client, message):
+    """
+    Handles the back navigation functionality when a user sends a message that matches the back trigger
+    and the message is in a private chat.
+
+    Args:
+        client: The client object.
+        message: The message object.
+
+    Returns:
+        None
+    """
     step = usercache.get_last_step(message.from_user.id)
     user_step = uxhandler.UXTree.nodes[step]
     if step == UserSteps.START.value:
