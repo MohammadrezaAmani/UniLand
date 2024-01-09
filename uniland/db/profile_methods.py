a4bee78fa10a528ea184d74f5d8bbdc35986cd99
Mohammadreza Amani <More.Amani@yahoo.com>
2024-01-09 12:34:23 +0330

    Add profile management docs to profile_methods.py

--- uniland/db/profile_methods.py
+++ uniland/db/profile_methods.py
@@ -1,35 +1,35 @@
+"""
+This module contains functions for managing profiles in the UniLand database.
+
+Classes:
+- Profile: Represents a user profile in the database.
+
+Functions:
+- add_profile(user_id: int) -> None: Adds a profile for the given user ID.
+- get_profile(id: int) -> Profile: Retrieves a profile from the database based on the given ID.
+- list_profiles() -> List[Profile]: Retrieves a list of all profiles from the database.
+"""
+
 import threading
+from typing import List
 
 from uniland import SESSION
 from uniland.db import user_methods as user_db
 from uniland.db.tables import Profile
 
-"""
-	Submission Class Properties:
-		- id: int
-		- submission_data : datetime
-		- is_confirmed : bool
-		- faculty : str
-		- search_text : str
-		- description : str
-		- correspondent_admin : int --> fk user.user_id
-		- owner : int --> fk user.user_id
-
-	Profile Class Properties:
-		* Inherits from Submissions with type='profile'
-		- title: str --> nullable=False
-		- email: str
-		- phone_number: str
-		- image_link: str
-		- image_id: str
-		- resume_link : str
-		- resume_id: int
-"""
-
 PROFILE_INSERTION_LOCK = threading.RLock()
 
 
-def add_profile(user_id):
+def add_profile(user_id: int) -> None:
+    """
+    Adds a profile for the given user ID.
+
+    Args:
+            user_id (int): The ID of the user.
+
+    Returns:
+            None
+    """
     global cnt
     user = user_db.add_user(user_id)
 
@@ -41,12 +41,27 @@ def add_profile(user_id):
         SESSION.close()
 
 
-def get_profile(id: int):
+def get_profile(id: int) -> Profile:
+    """
+    Retrieves a profile from the database based on the given ID.
+
+    Args:
+            id (int): The ID of the profile to retrieve.
+
+    Returns:
+            Profile: The retrieved profile object.
+    """
     profile = SESSION.query(Profile).filter(Profile.id == id).first()
     SESSION.expunge(profile)
     SESSION.close()
     return profile
 
 
-def list_profiles():
+def list_profiles() -> List[Profile]:
+    """
+    Retrieves a list of all profiles from the database.
+
+    Returns:
+            List[Profile]: A list of Profile objects.
+    """
     return SESSION.query(Profile).all()
